552dcb9ca60de9ca21647c0ab21f0bc56201a480
Rosario <rosarioterranova91@gmail.com>
2020-08-19 16:08:35 +0200

    aggiornamento API

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const getEveryDayData = async (country) =>{
+const getData = async () =>{
     try{
-        const response = await fetch("https://api.covid19api.com/dayone/country/"+country)
+        const response = await fetch("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json")
         const data = await response.json()
         return data
     }
@@ -10,34 +10,31 @@ const getEveryDayData = async (country) =>{
     }
 }
 
-const getDates = (dataObj) => {
-    let date = []
+// const getDate = (dataObj) => {
+//     let date = []
+//     dataObj.forEach(element => {
+//         date.push(new Date(element.data).toLocaleDateString())
+//     })
+//     return date;
+// }
+
+const getInfo = (dataObj, param) => {
+    let info = []
     dataObj.forEach(element => {
-        date.push(new Date(element.Date).toLocaleDateString())
+        info.push(element[param])
     })
-    return date;
+    return info;
 }
 
-const getConfirmed = (dataObj) => {
-    let confirmed = []
-    let oldCondifermed = 0;
-    dataObj.forEach(element => {
-        let newCases = element.Confirmed - oldCondifermed;
-        oldCondifermed += newCases;
-        confirmed.push(newCases)
-    })
-    return confirmed;
-}
-
-const renderChart = (dates, confirmed) =>{
+const renderChart = (dataObj) =>{
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: dates,
+            labels: getInfo(dataObj,"data").map(x => new Date(x).toLocaleDateString()),
             datasets: [{
                 label: 'Contagiati',
-                data: confirmed,
+                data: getInfo(dataObj,"nuovi_positivi"),
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -69,25 +66,30 @@ const renderChart = (dates, confirmed) =>{
     });
 }
 
-const renderTable = (dates, confirmed) =>{
+const renderTable = (dataObj) =>{
     const table = document.querySelector("#table-elements")
-    for (let index = dates.length-1; index > 0; index--) {
+    for (let index = dataObj.length -1; index > 0; index--) {
         table.innerHTML += `
         <tr>
-            <th scope="row">${dates[index]}</th>
-            <td>${confirmed[index]}</td>
+            <th scope="row">${new Date(dataObj[index].data).toLocaleDateString()}</th>
+            <td>${dataObj[index].ricoverati_con_sintomi}</td>
+            <td>${dataObj[index].terapia_intensiva}</td>
+            <td>${dataObj[index].totale_ospedalizzati}</td>
+            <td>${dataObj[index].isolamento_domiciliare}</td>
+            <td><b>${dataObj[index].nuovi_positivi}</b></td>
+            <td>${dataObj[index].totale_positivi}</td>
+            <td>${dataObj[index].dimessi_guariti}</td>
+            <td>${dataObj[index].deceduti}</td>
+            <td>${dataObj[index].tamponi}</td>
         </tr>
         `
     }
 }
 
 const init = async () => {
-    let dataObj = await getEveryDayData("italy");
-    console.log(dataObj)
-    let dates = getDates(dataObj)
-    let confirmed = getConfirmed(dataObj)
-    renderChart(dates, confirmed);
-    renderTable(dates, confirmed);
+    let dataObj = await getData();
+    renderChart(dataObj)
+    renderTable(dataObj)
 }
 
 init()
