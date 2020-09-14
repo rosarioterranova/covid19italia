5a5766deeed7c52451b348783f25b98ffdefce09
Rosario <rosarioterranova91@gmail.com>
2020-09-14 13:07:11 +0200

    switched to vuejs

--- app.js
+++ app.js
@@ -1,14 +1,99 @@
-const getData = async () =>{
-    try{
-        const response = await fetch("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json")
-        const data = await response.json()
-        return data
+Vue.component("info",{
+    template:`
+    <div class="jumbotron text-center">
+      <h1 class="display-4">Covid-19 Italia</h1>
+      <p class="lead">Strumento online open source per monitorare giorno per giorno il numero dei contagiati di <a href="https://it.wikipedia.org/wiki/COVID-19">COVID-19</a> in Italia.</p>
+      <hr>
+      <p>
+          Web App di <a href="http://rosarioterranova.com">Rosario Terranova</a> |
+          Dati Ufficiali <a href="https://github.com/pcm-dpc/COVID-19">Presidenza del Consiglio dei Ministri - Dipartimento della Protezione Civile</a> |
+          Grafici di <a href="https://www.chartjs.org/">ChartJS</a>
+      </p>
+    </div>
+    `
+})
+
+Vue.component("spinner", {
+    template:`
+    <div class="container text-center spinner-container">
+      <div class="spinner-border" role="status" style="width: 5rem; height: 5rem;">
+        <span class="sr-only">Caricamento...</span>
+      </div>
+      <p>Caricamento...</p>
+    </div>
+    `
+})
+
+Vue.component("chart",{
+    template:`
+    <div class="chart-container text-center data-showed">
+        <canvas id="myChart"></canvas>
+    </div>
+    `
+})
+
+Vue.component("data-table", {
+    template:`
+    <div class="table-responsive-sm my-5 data-showed">
+        <table class="table table-striped table-sm ">
+            <thead>
+              <tr>
+                <th>Data</th>
+                <th>Ricoverati con sintomi</th>
+                <th>Terapia intensiva</th>
+                <th>Tot. ospedalizzati</th>
+                <th>Isolamento domiciliare</th>
+                <th>Tot. positivi</th>
+                <th>Variazione tot. positivi</th>
+                <th>Nuovi positivi</th>
+                <th>Dimessi guariti</th>
+                <th>Deceduti</th>
+                <th>Casi da sospetto diagnostico</th>
+                <th>Casi da screening</th>
+                <th>Tot. casi</th>
+                <th>Tamponi</th>
+                <th>Casi testati</th>
+              </tr>
+            </thead>
+            <tbody id="table-elements">
+            </tbody>
+          </table>
+    </div>
+    `
+})
+
+Vue.component("all-data", {
+    template:`
+    <div>
+        <spinner v-show="!isDataReady"></spinner>
+        <chart v-show="isDataReady"></chart>
+        <data-table v-show="isDataReady"></data-table>
+    </div>
+    `,
+    data(){
+        return{
+            data: [],
+            isDataReady: false
+        }
+    },
+    created(){
+        this.getData()
+    },
+    methods:{
+        async getData(){
+            try{
+                const response = await fetch("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json")
+                this.data = await response.json()
+                this.isDataReady = true
+                renderChart(this.data)
+                renderTable(this.data)
+            }
+            catch{
+                this.data = {error: "error getting data"}
+            }
+        }
     }
-    catch{
-        alert("Errore: dati non disponibili, riprova più tardi")
-        return {}
-    }
-}
+})
 
 const getInfo = (dataObj, param) => {
     let info = []
@@ -18,9 +103,9 @@ const getInfo = (dataObj, param) => {
     return info;
 }
 
-const renderChart = (dataObj) =>{
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+const renderChart = (dataObj) => {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: getInfo(dataObj,"data").map(x => new Date(x).toLocaleDateString()),
@@ -83,12 +168,6 @@ const renderTable = (dataObj) =>{
     }
 }
 
-const init = async () => {
-    let dataObj = await getData();
-    document.querySelectorAll(".data-showed").forEach(item => item.classList.remove("d-none"))
-    document.querySelector(".spinner-container").classList.add("d-none")
-    renderChart(dataObj)
-    renderTable(dataObj)
-}
-
-init()
+const app = new Vue({
+    el:"#app"
+})
