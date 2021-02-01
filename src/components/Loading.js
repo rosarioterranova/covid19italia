234f21d42beecc582140b441f0acbc75b52d99a3
Rosario Terranova <rosarioterranova91@gmail.com>
2021-02-01 12:11:36 +0100

    upload website

--- src/components/Loading.js
+++
@@ -1,8 +0,0 @@
-export default function Loading(){
-    return(
-        <div className="text-center">
-            <div className="spinner-border" role="status"></div>
-            <p>Caricamento dati...</p>
-        </div>
-    )
-}
